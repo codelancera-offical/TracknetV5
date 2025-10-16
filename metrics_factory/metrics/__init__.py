2e37e0ee74df862ee2cca513d865d1cc9bcb258d
mschen25 <646476489@qq.com>
2025-10-16 17:01:48 +0800

    add(init):添加init中的部分参数

--- metrics_factory/metrics/__init__.py
+++ metrics_factory/metrics/__init__.py
@@ -1,7 +1,7 @@
 from .utracknetv1_metric import UTrackNetV1Metric
-
+from .utracknetv2_metric import UTrackNetV2Metric
 
 __all__ = [
-    'UTrackNetV1Metric'
-
+    'UTrackNetV1Metric',
+    'UTrackNetV2Metric'
     ]
--- models_factory/heads/__init__.py
+++ models_factory/heads/__init__.py
@@ -1,9 +1,10 @@
 from .utracknetv1_head import UTrackNetV1Head
 from .inpaintnet_head import InpaintNetHead
 from .tracknetv3_head import TrackNetV3Head
-
+from .utracknetv1_head_sigmoid import UTrackNetV1HeadSigmoid
 __all__ = [
     'UTrackNetV1Head',
     'InpaintNetHead',
-    'TrackNetV3Head'
+    'TrackNetV3Head',
+    'UTrackNetV1HeadSigmoid'
 ]
