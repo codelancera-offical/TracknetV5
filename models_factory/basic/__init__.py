93876473fb5be3398dcc290bf394bf2be40f56e0
HaoNan Tang <lancera.thn@gmail.com>
2025-10-26 20:43:23 +0800

    feat(v2-mvdr) add learnable mvdr module

--- models_factory/basic/__init__.py
+++ models_factory/basic/__init__.py
@@ -2,10 +2,12 @@ from .conv_block import BasicConvBlock
 from .dws_conv_block import DepthwiseSeparableConvBlock
 from .attention import CBAM, SpatialAttention
 from .attention import ChannelAttention
+from .mv_dr_attention import MotionDirectionAttentionLayer
 
 __all__ = [
     "BasicConvBlock",
     "DepthwiseSeparableConvBlock",
     'CBAM',
-    "SpatialAttention"
+    "SpatialAttention",
+    'MotionDirectionAttentionLayer'
 ]
