04b624f7d90dbc351c9f483490176ab426951708
mschen25 <646476489@qq.com>
2025-10-26 11:33:23 +0800

    add(init):添加tracknetv4注册

--- models_factory/heads/__init__.py
+++ models_factory/heads/__init__.py
@@ -5,6 +5,7 @@ from .utracknetv1_head_sigmoid import UTrackNetV1HeadSigmoid
 from .utracknetv1_dws_head_sigmoid import UTrackNetV1DWSHeadSigmoid
 from .tracknetv1_head import TrackNetV1Head
 from .tracknetv2_head import TrackNetV2Head
+from .tracknetv4_head import TrackNetV4Head
 __all__ = [
     'UTrackNetV1Head',
     'InpaintNetHead',
@@ -12,5 +13,6 @@ __all__ = [
     'UTrackNetV1HeadSigmoid',
     'UTrackNetV1DWSHeadSigmoid',
     'TrackNetV1Head',
-    'TrackNetV2Head'
+    'TrackNetV2Head',
+    'TrackNetV4Head'
 ]
--- models_factory/models/__init__.py
+++ models_factory/models/__init__.py
@@ -3,9 +3,11 @@ from .tracknetv3 import TrackNetV3
 from .inpaintnet import InpaintNet
 from .tracknetv1 import TrackNetV1
 from .tracknetv2 import TrackNetV2
+from .tracknetv4 import TrackNetV4
 __all__ = [
     'UTrackNetV1',
     'TrackNetV3',
     'InpaintNet',
-    'TrackNetV2'
+    'TrackNetV2',
+    'TrackNetV4'
 ]
