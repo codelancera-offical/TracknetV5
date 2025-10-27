16cef14353178aae01e3ab58273667eb52a641c4
thaonan-rog-windows <lancera.thn@gmail.com>
2025-10-27 13:29:20 +0800

    feat(mvdr) learnable res start train, suggest stop at this branch

--- configs/experiments/tracknetv2_mvdr-attention-res_tennis_b2e500.py
+++ configs/experiments/tracknetv2_mvdr-attention-res_tennis_b2e500.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 # ------------------- 1. 模型定义 (Model) -------------------
 model = dict(
-    type='TrackNetV2LRMVDR',
+    type='TrackNetV2MVDR',
     backbone=dict(
-        type='TrackNetV2LRMVDRBackbone',
+        type='TrackNetV2MVDRBackbone',
         in_channels=13
     ),
     neck=dict(
